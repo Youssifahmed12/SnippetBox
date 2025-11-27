64f410033b1760b64ecfbe316bb7b901342ad499
Youssifahmed12 <youssefbro12397@gmail.com>
2025-11-27 21:31:16 +0200

    new routes without trailing slashes

--- main.go
+++ main.go
@@ -8,9 +8,18 @@ import (
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippet Box"))
 }
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a snippet..."))
+}
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display Create Snippet Form.."))
+}
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("listening on port :4000")
 
