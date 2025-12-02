eb7a5cf59e20693c62b691eae9b6ee2dc04e0dcf
Youssifahmed12 <youssefbro12397@gmail.com>
2025-12-03 00:02:18 +0200

    refactoring for common dynamic data

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -19,9 +19,10 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	app.render(w, r, http.StatusOK, "home.tmpl.html", templateData{
-		Snippets: snippets,
-	})
+	data := app.newTemplateData(r)
+	data.Snippets = snippets
+
+	app.render(w, r, http.StatusOK, "home.tmpl.html", data)
 
 }
 
@@ -41,6 +42,9 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+
+	data := app.newTemplateData(r)
+	data.Snippet = snippet
 
 	app.render(w, r, http.StatusOK, "view.tmpl.html", templateData{Snippet: snippet})
 }
--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"html/template"
+	"net/http"
 	"path/filepath"
+	"time"
 
 	"github.com/youssifahmed12/snippetbox/internal/models"
 )
 
 type templateData struct {
-	Snippet  models.Snippet
-	Snippets []models.Snippet
+	CurrentYear int
+	Snippet     models.Snippet
+	Snippets    []models.Snippet
 }
 
 func newTemplateCache() (map[string]*template.Template, error) {
@@ -41,4 +44,8 @@ func newTemplateCache() (map[string]*template.Template, error) {
 		cache[name] = ts
 	}
 	return cache, nil
+}
+
+func (app *application) newTemplateData(r *http.Request) templateData {
+	return templateData{CurrentYear: time.Now().Year()}
 }
