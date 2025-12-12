414e702f8090a4eb93ce935b2adae6f703c11f93
Youssifahmed12 <youssefbro12397@gmail.com>
2025-12-12 20:22:16 +0200

    quick fix

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -57,5 +57,8 @@ func newTemplateCache() (map[string]*template.Template, error) {
 }
 
 func (app *application) newTemplateData(r *http.Request) templateData {
-	return templateData{CurrentYear: time.Now().Year()}
+	return templateData{
+		CurrentYear: time.Now().Year(),
+		Flash:       app.sessionManager.PopString(r.Context(), "flash"),
+	}
 }
